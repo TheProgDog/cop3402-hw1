eec98db8e7891b8f4c96e52ba65df733e1237a92
Daniel Hernandez-Otero <danielhotero96@knights.ucf.edu>
2021-01-30 14:58:24 -0500

    First commit
    Reads out the input from input.txt

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+  FILE *input = fopen("input.txt", "r");
+  FILE *output = fopen("output.txt", "w");
+
+  char reader = 'a';
+
+  printf("The input is:\n\n");
+
+  while (!feof(input))
+  {
+    fscanf(input, "%c", &reader);
+
+    printf("%c", reader);
+  }
+
+  printf("\n\nOutput:\n\n");
+
+  fclose(input);
+  fclose(output);
+}
